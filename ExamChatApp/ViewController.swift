b8b752f26836e9c6a95a41dedbe98387bc60aa85
Casper <ukdtom@Error-404.local>
2019-05-14 15:51:55 +0200

    first commit

---
+++ ExamChatApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ExamChatApp
+//
+//  Created by Tommy Mikkelsen on 14/05/2019.
+//  Copyright © 2019 Casper Sillemann. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        FirebaseApp.configure()
+    }
+
+
+}
+
